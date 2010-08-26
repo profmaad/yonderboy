845b57be1928aa6112f5c15aac756fee70324122
Prof <himself@prof-maad.org>
2010-08-26 23:35:47 +0200

    * the ruby test viewer now displays stati - somewhat

--- testclients/viewer.rb
+++ testclients/viewer.rb
@@ -19,6 +19,29 @@ def parsePacket
     elsif(@buffer["command"] == "disconnect-from-renderer")
       @gtkSocket.add_id(0)
     end
+  elsif(@buffer["type"] == "status-change")
+    case @buffer["status"]
+    when "load-started"
+      @statusbar.pop(@statusbarContextRendererLoad)
+      @statusbar.push(@statusbarContextRendererLoad, "started loading "+@buffer["uri"])
+    when "load-finished"
+      @statusbar.pop(@statusbarContextRendererLoad)
+      @statusbar.push(@statusbarContextRendererLoad, "done loading "+@buffer["uri"])
+    when "load-failed"
+      @statusbar.pop(@statusbarContextRendererLoad)
+      @statusbar.push(@statusbarContextRendererLoad, "loading failed")
+    when "progress-changed"
+      progress = @buffer["progress"].to_f
+      percentage = (progress*100).to_i
+
+      @statusbar.pop(@statusbarContextRendererLoad)
+      @statusbar.push(@statusbarContextRendererLoad, "#{percentage}% loaded")
+    when "hovering-over-link"
+      @statusbar.pop(@statusbarContextRendererHover)
+      @statusbar.push(@statusbarContextRendererHover, @buffer["uri"]+" ("+@buffer["title"]+")")      
+    when "not-hovering-over-link"
+      @statusbar.pop(@statusbarContextRendererHover)
+    end
   elsif(@buffer["type"] == "ack" && @buffer["id"] == "0")
     @socket.write("type = connection-management\ncommand = register-view\nview-id = view00\ndisplay-information = #{@gtkSocket.id}\ndisplay-information-type = XEMBED\nid = 1\n\n")
   end
@@ -32,6 +55,8 @@ end
 
 # GUI elements
 @gtkSocket = Gtk::Socket.new
+@statusbar = Gtk::Statusbar.new
+@vbox = Gtk::VBox.new
 @window = Gtk::Window.new
 
 @window.signal_connect("destroy"){Gtk.main_quit}
@@ -40,8 +65,18 @@ end
   @window.show_all
 end
 
-@window.add(@gtkSocket)
+@vbox.pack_start(@gtkSocket, true, true)
+@vbox.pack_end(@statusbar, false, false)
+@window.add(@vbox)
 @window.show_all
+
+# init statusbar context
+@statusbarContextLocal = @statusbar.get_context_id("23")
+@statusbarContextServer = @statusbar.get_context_id("server")
+@statusbarContextRendererLoad = @statusbar.get_context_id("renderer-load")
+@statusbarContextRendererHover = @statusbar.get_context_id("renderer-hover")
+
+@statusbar.push(@statusbarContextLocal, "Welcome to gtk-viewer on yonderboy 0.0")
 # TEMP END
 
 # IPC socket stuff
