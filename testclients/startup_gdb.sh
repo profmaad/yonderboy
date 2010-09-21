94d046c66271cae559f77a2fe05af531c039c21b
Prof <himself@prof-maad.org>
2010-09-21 14:52:55 +0200

    * seperated the startup.sh script into a debugger and non-debugger version

--- testclients/startup_gdb.sh
+++ testclients/startup_gdb.sh
@@ -8,8 +8,9 @@ CONTROLLER="controllers/cli-controller/src/cli-controller"
 VIEWER="viewers/tabbed-gtk-viewer/src/tabbed-gtk-viewer"
 CONFIG="/home/profmaad/.cli-browser/config"
 
-$TERMINAL $WORKDIR$SERVER -c $CONFIG &
-sleep 1
+$TERMINAL gdb --args $WORKDIR$SERVER -c $CONFIG &
+#sleep 5
+read
 $TERMINAL $WORKDIR$CONTROLLER $CONFIG &
 $TERMINAL $WORKDIR$VIEWER $CONFIG &
 
